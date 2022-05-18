7c607145f963ea784f7b8e8561de62901805a053
Martijn-Alyanak <60792074+martijnlammers@users.noreply.github.com>
2022-05-18 13:38:30 +0200

    Added robot controller

--- controllers/supervisor_controller/build/release/supervisor_controller.d
+++ controllers/supervisor_controller/build/release/supervisor_controller.d
@@ -5,5 +5,4 @@ build/release/supervisor_controller.o: supervisor_controller.cpp \
  C:\Program\ Files\Webots/include/controller/cpp/webots/Node.hpp \
  C:\Program\ Files\Webots/include/controller/cpp/webots/Field.hpp \
  C:/Program\ Files/Webots/include/controller/c/webots/contact_point.h \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp
--- controllers/supervisor_controller/supervisor_controller.cpp
+++ controllers/supervisor_controller/supervisor_controller.cpp
@@ -1,50 +1,38 @@
 #include <webots/Supervisor.hpp>
-#include <webots/Motor.hpp>
-#include <string>
-#include <iostream>
-#include <cstdlib>
 
-#define TIME_STEP 64
-#define MAX_SPEED 10.28
-#define LEFTMOTOR 0
-#define RIGHTMOTOR 1
+#define TIME_STEP 32
 
 using namespace webots;
-using namespace std;
 
-Supervisor *supervisor = new Supervisor();
-Node *robot_node;
-
-void rotateRobot(const string &name, int degrees){
-  robot_node = supervisor->getFromDef(name);
-  Field *r_field = robot_node->getField("rotation");
-  
-  //Get the rotation values from the node field.
-  const double *r_val = r_field->getSFRotation();
-  const double new_r_val[4] = {r_val[0], r_val[1], r_val[2], (r_val[3] + (degrees * 0.0174532925))};
-  r_field->setSFRotation(new_r_val);
-}
-
-void moveToLocation(const string &name, double x, double y){
-  robot_node = supervisor->getFromDef(name);
-  if (robot_node == NULL) exit(1);
-   
-  Field *trans_field = robot_node->getField("translation");
-  const double *values = trans_field->getSFVec3f();
-  cout << "MY_ROBOT is at position: " << values[0] << ' ' << values[1] << ' ' << values[2] << endl;  
-  const double newValue[3] = {x, y, values[2]};
-  trans_field->setSFVec3f(newValue);
-}
-  
-  
 int main() {
-  double x = -0.52;
-  double y = -0.44;
-  // while (supervisor->step(TIME_STEP) != -1) {
-        // moveBot("red", x, y); 
-  // }
-  rotateRobot("red", 180);
-  //moveToLocation("red", x, y); 
+  Supervisor *supervisor = new Supervisor();
+
+  // do this once only
+  Node *robot_node = supervisor->getFromDef("red");
+  Node *robot_node1 = supervisor->getFromDef("green");
+  Node *robot_node2 = supervisor->getFromDef("blue");
+  if (robot_node == NULL) {
+    std::cerr << "No DEF MY_ROBOT node found in the current world file" << std::endl;
+    exit(1);
+  }
+  Field *trans_field = robot_node->getField("translation");
+  Field *trans_field1 = robot_node1->getField("translation");
+  Field *trans_field2 = robot_node2->getField("translation");
+
+  while (supervisor->step(TIME_STEP) != -1) {
+    // this is done repeatedly
+    const double *values = trans_field->getSFVec3f();
+    std::cout << "MY_ROBOT is at position: " << values[0] << ' '
+              << values[1] << ' ' << values[2] << std::endl;
+    const double *values1 = trans_field1->getSFVec3f();
+    std::cout << "MY_ROBOT1 is at position: " << values1[0] << ' '
+              << values1[1] << ' ' << values1[2] << std::endl;
+    const double *values2 = trans_field2->getSFVec3f();
+    std::cout << "MY_ROBOT2 is at position: " << values2[0] << ' '
+              << values2[1] << ' ' << values2[2] << std::endl;
+          
+  }
+
   delete supervisor;
   return 0;
 }
