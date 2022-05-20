0f7694a5424e1f67327f9c168389b16d1906180f
M. Lammers <60792074+martijnlammers@users.noreply.github.com>
2022-05-20 17:39:10 +0200

    Added sensors

--- controllers/controller/build/release/controller.d
+++ controllers/controller/build/release/controller.d
@@ -1,6 +1,8 @@
 build/release/controller.o: controller.cpp \
  C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Accelerometer.hpp \
  C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
  C:/Program\ Files/Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Compass.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/GPS.hpp
--- controllers/controller/controller.cpp
+++ controllers/controller/controller.cpp
@@ -1,14 +1,16 @@
 #include <webots/Robot.hpp>
-#include <webots/DistanceSensor.hpp>
+#include <webots/Accelerometer.hpp>
 #include <webots/Motor.hpp>
+#include <webots/Compass.hpp>
+#include <webots/GPS.hpp>
 #include <string>
 #include <iostream>
 
 // time in [ms] of a simulation step
 #define TIME_STEP 64
 #define MAX_SPEED 10.28
-#define LEFTMOTOR 0
-#define RIGHTMOTOR 1
+#define LEFT 0
+#define RIGHT 1
 #define NUM_MOTORS 2
 #define NEGATIVE_DEG degrees < 0
 #define POSITIVE_DEG degrees > 0
@@ -17,73 +19,54 @@
 using namespace webots;
 using namespace std;
 
-Robot *robot = new Robot();
-Motor *leftMotor = robot->getMotor("wheel_left");
-Motor *rightMotor = robot->getMotor("wheel_right");
-enum string_code{eForward,
-  eBackward, eRotate, eInvalid
-};
+Robot *r = new Robot();
+Motor *motors[2] = {r->getMotor("wheel_left"), r->getMotor("wheel_right")};
+GPS *gps = r->getGPS("global");
+//Accelerometer *accMeter = r->getAccelerometer("accelerometer");
+Compass *compass = r->getCompass("compass");
 
-// In a function as its used in an overload.
-string_code encodeDirection(string direction){
-  string_code code = (direction == "FORWARD") ? eForward :
-  (direction == "BACKWARD") ? eBackward : 
-  (direction == "ROTATE") ? eRotate : eInvalid;
-  return code;
+
+// Motor *rightMotor = robot->getMotor("wheel_right");
+// enum string_code{eForward,
+  // eBackward, eRotate, eInvalid
+// };
+void initializeRobot(){
+  gps->enable(TIME_STEP);
+  //accMeter->enable(TIME_STEP);
+  compass->enable(TIME_STEP);
+  
+}
+void rotateToTarget(float x, float y){
+  // Current position
+  // Make vector
+  // calculate orientation
+  // 
 }
 
-void changeDirection(Robot *robot, string direction){
-  leftMotor->setPosition(INFINITY);
-  rightMotor->setPosition(INFINITY);
-  switch(encodeDirection(direction)){
-  case eForward:
-     leftMotor->setVelocity(0.1 * MAX_SPEED);
-     rightMotor->setVelocity(0.1 * MAX_SPEED);
-    
-    cout << "FORWARD"<< endl;
-    break;
-  case eBackward:
-     leftMotor->setVelocity(-(0.1 * MAX_SPEED));
-     rightMotor->setVelocity(-(0.1 * MAX_SPEED));
-    cout << "BACKWARD"<< endl;
-    break;
+void moveRobot(float x, float y){
+     motors[LEFT]->setPosition(INFINITY);
+     motors[RIGHT]->setPosition(INFINITY);
+     motors[LEFT]->setVelocity(0.1 * MAX_SPEED);
+     motors[RIGHT]->setVelocity(-0.1 * MAX_SPEED);
+     cout << "coordinates: " << gps->getValues()[0] << endl;
+     cout << "coordinates: " << gps->getValues()[1] << endl;
+     cout << "coordinates: " << gps->getValues()[2] << endl;
+     // cout << "accelerometer: " << accMeter->getValues()[0] << endl;
+     // cout << "accelerometer: " << accMeter->getValues()[1] << endl;
+     // cout << "accelerometer: " << accMeter->getValues()[2] << endl;
+     cout << "compass: " << compass->getValues() << endl;
+  
+     rotateToTarget(x, y);
      
-  default:
-    leftMotor->setVelocity(0);
-    rightMotor->setVelocity(0);
-    break;
-  }
-  
 }
 
-void changeDirection(Robot *robot, string direction, int degrees){
-  
-  leftMotor->setPosition(INFINITY);
-  rightMotor->setPosition(INFINITY);
-  switch(encodeDirection(direction)){
-  case eRotate:
-    cout << "ROTATE, Rotation in deg: " << degrees << endl;
-    if(POSITIVE_DEG){
-      leftMotor->setVelocity(0.1 * MAX_SPEED);
-      rightMotor->setVelocity(-0.1 * MAX_SPEED);
-    } else if(NEGATIVE_DEG){
-      cout << "ROTATE, Rotation in deg: " << degrees << endl;
-      leftMotor->setVelocity(-0.1 * MAX_SPEED);
-      rightMotor->setVelocity(0.1 * MAX_SPEED);
-    }
-    
-    break;
-  default:
-    leftMotor->setVelocity(0);
-    rightMotor->setVelocity(0);
-    break;
-  }
-}
-
-
-// entry point of the controller
 int main(int argc, char **argv) {
-   changeDirection(robot, "ROTATE", 90);
-   delete robot;
+   initializeRobot();
+   float x = -0.06, y = -0.06;
+   while(r->step(TIME_STEP) != -1){
+     moveRobot(x, y);
+   }
+   
+   delete r;
    return 0; //EXIT_SUCCESS
 }
