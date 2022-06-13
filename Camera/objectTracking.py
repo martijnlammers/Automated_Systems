6c10b60888d562a961691ed76a5b48aae5c83aac
rtsmoor <57296077+rtsmoor@users.noreply.github.com>
2022-06-13 15:47:28 +0200

    Add tracker choices

--- Camera/objectTracking.py
+++ Camera/objectTracking.py
@@ -13,3 +13,31 @@ class Grid():
         self.frameWidth = frameWidth
         self.gridDefined = False
 
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+# Set up tracker.
+# Instead of MIL, you can also use
+
+tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+tracker_type = tracker_types[7] #currently using CSRT since it seems to behave the way we want it to
+
+if int(minor_ver) < 3:
+    tracker = cv2.Tracker_create(tracker_type)
+else:
+    if tracker_type == 'BOOSTING':
+        tracker = cv2.TrackerBoosting_create()
+    if tracker_type == 'MIL':
+        tracker = cv2.TrackerMIL_create()
+    if tracker_type == 'KCF':
+        tracker = cv2.TrackerKCF_create()
+    if tracker_type == 'TLD':
+        tracker = cv2.TrackerTLD_create()
+    if tracker_type == 'MEDIANFLOW':
+        tracker = cv2.TrackerMedianFlow_create()
+    if tracker_type == 'GOTURN':
+        tracker = cv2.TrackerGOTURN_create()
+    if tracker_type == 'MOSSE':
+        tracker = cv2.TrackerMOSSE_create()
+    if tracker_type == "CSRT":
+        tracker = cv2.TrackerCSRT_create()
+
