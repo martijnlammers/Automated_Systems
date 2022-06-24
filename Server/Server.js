8df08858b66c684fb0f6e5e465e1b7494fe47167
Server <0986902@hr.nl>
2022-06-24 09:07:30 +0000

    Added robotDetected function

--- Server/Server.js
+++ Server/Server.js
@@ -1,17 +1,19 @@
 const robotLib = require("./Robot");
 const communication = require("./Communication");
 const actions = require("./Actions");
-let restart = 0;
 
 var commands = {
   "rotateAck": actions.rotateOrDrive,
   "obstacleDetected": actions.obstacleDetected,
   "goalDetected": actions.goalDetected,
+  "robotDetected": actions.robotDetected,
   "begin": actions.begin,
   "position": actions.position
 };
 
 function inputHandler(robot, topicArray, message){
+  if (topicArray[3] === "robotDetected") robot["obstacleDetected"] = message.split(",").map(Number);
+
   if(Object.getOwnPropertyNames(robot).some(property => property == topicArray[3])){
     switch(typeof(robot[topicArray[3]])){
       case "number":
@@ -29,6 +31,7 @@ function inputHandler(robot, topicArray, message){
         break;
       case "string":
         robot[topicArray[3]] = message;
+        console.log("string")
         break;
     }
   }
@@ -52,7 +55,7 @@ communication.client.on('connect', () => {
 communication.client.on("message", (topic, message) => {
   let splitTopic = topic.split('/');
   message = message.toString();
-
+  
   if(!(splitTopic[0] === "robots" && splitTopic[1] === "toServer" && splitTopic.length > 2)) return;
 
   if(robotLib.robots.some(r => r.robotID === splitTopic[2])){
@@ -68,5 +71,3 @@ communication.client.on("message", (topic, message) => {
   if(action.length === 0) action();
   else if(robot !== null || robot !== undefined) action(robot);
 });
-
-
